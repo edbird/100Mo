5b74c4e0f3a8f874d7a889e375b15d548b095722
ecb <ecb@hostname.hep.manchester.ac.uk>
2018-03-27 16:16:00 +0100

    main.cpp calculates ratio and produces output

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #include "TCanvas.h"
 #include "TH1.h"
+#include "TH2.h"
 #include "TFile.h"
 
 
@@ -29,11 +30,73 @@ void read_phase_space_factor(const char* buffer, double &value)
 
 }
 
+template <typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<std::vector<T>> v)
+{
+    typename std::vector<std::vector<T>>::const_iterator it{v.cbegin()};
+    for(; it != v.cend(); ++ it)
+    {
+        os << *it << "\n";
+    }
+    return os;
+}
+
+template <typename T>
+std::ostream& operator<<(std::ostream& os, std::vector<T> v)
+{
+    typename std::vector<T>::const_iterator it{v.cbegin()};
+    for(; it != v.cend(); ++ it)
+    {
+        os << *it << " ";
+    }
+    return os;
+}
+
+void read_data(const char* buffer, std::vector<std::vector<double>>& data)
+{
+    
+    data.clear();
+    data.emplace_back(std::vector<double>());
+
+    //std::cout << buffer << std::endl;
+
+    const char* p{buffer};
+    char* p_end;
+    while(*p != '\0')
+    {
+        while(*p == ' ') ++ p;
+        double value{strtod(p, &p_end)};
+        if(p == p_end)
+        {
+            data.pop_back();
+            break;
+        }
+        data.back().emplace_back(value);
+        p = p_end;
+        if(*p == '\n')
+        {
+            //std::cout << data.at(data.size() - 1) << std::endl;
+            data.emplace_back(std::vector<double>());
+        }
+    }
+
+    //std::cout << "data read" << std::endl;
+    //std::cout << "dimension x: " << data.size() << std::endl;
+    //for(std::size_t i{0}; i < data.size(); ++ i)
+    //{
+    //    std::cout << "x=" << i << " dimension y: " << data[i].size() << std::endl;
+    //    std::cin.get();
+    //}
+    //std::cout << "dimension y: " << data.at(0).size() << std::endl;
+
+}
+
 int main()
 {
 
     // ifstream objects
     std::ifstream ifs_nEqNull("nEqNull.dat", std::ios::ate);
+    //std::ifstream ifs_nEqNull("test.dat", std::ios::ate);
     std::ifstream ifs_nEqTwo("nEqTwo.dat", std::ios::ate);
 
     std::ifstream ifs_psiN0("psiN0.txt", std::ios::ate);
@@ -92,8 +155,77 @@ int main()
 
     // read data
     read_data(buf_nEqNull, data_nEqNull);
+    std::cout << "Finished reading " << "nEqNull.dat" << std::endl;
     read_data(buf_nEqTwo, data_nEqTwo);
+    std::cout << "Finished reading " << "nEqTwo.dat" << std::endl;
+
+    //std::cout << buf_nEqNull << std::endl;
+    //std::cout << buf_nEqTwo << std::endl;
+
+    std::vector<std::vector<double>> ratio;
+    ratio.resize(data_nEqNull.size());
+    for(std::size_t i{0}; i < ratio.size(); ++ i)
+    {
+        ratio[i].resize(data_nEqNull[i].size());
+    }
+
+    const double epsilon_31{0.8};
+    for(std::size_t i{0}; i < ratio.size(); ++ i)
+    {
+        for(std::size_t j{0}; j < 2 /*ratio[i].size()*/; ++ j)
+        {
+            ratio[i][j] = data_nEqNull[i][j];
+        }
+        ratio[i][2] = (data_nEqNull[i][2] + epsilon_31 * data_nEqTwo[i][2]) / data_nEqNull[i][2];
+    }
+    std::cout << "Finished constructing ratio" << std::endl;
+
+    //std::cout << ratio << std::endl;
+
+    Int_t dimension_xy{1001};
+    TH2D *h_nEqNull = new TH2D("h_nEqNull", "", dimension_xy, 0.0, 1.0, dimension_xy, 0.0, 1.0);
+    TH2D *h_nEqTwo = new TH2D("h_nEqTwo", "", dimension_xy, 0.0, 1.0, dimension_xy, 0.0, 1.0);
+    TH2D *h_ratio = new TH2D("h_ratio", "", dimension_xy, 0.0, 1.0, dimension_xy, 0.0, 1.0);
+    h_ratio->SetStats(0);
+    h_nEqNull->SetStats(0);
+    h_nEqTwo->SetStats(0);
+
+    for(std::size_t i{0}; i < dimension_xy; ++ i)
+    {
+        for(std::size_t j{0}; j < dimension_xy; ++ j)
+        {
+            h_nEqNull->SetBinContent(i, j, data_nEqNull.at(i * dimension_xy + j)[2]);
+            h_nEqTwo->SetBinContent(i, j, data_nEqTwo.at(i * dimension_xy + j)[2]);
+            //if(i < dimension_xy - j)
+            if(i + j < dimension_xy - 1)
+            {
+                h_ratio->SetBinContent(i, j, ratio.at(i * dimension_xy + j)[2]);
+            }
+        }
+    }
+    std::cout << "Finished constructing histogram" << std::endl;
+
+    TCanvas *c_ratio = new TCanvas("c_ratio", "", 4000, 3000);
+    h_ratio->Draw("colz");
+    c_ratio->SaveAs("c_ratio.png");
+    c_ratio->SaveAs("c_ratio.pdf");
+    c_ratio->SaveAs("c_ratio.C");
+    delete c_ratio;
     
+    TCanvas *c_nEqNull = new TCanvas("c_nEqNull", "", 4000, 3000);
+    h_nEqNull->Draw("colz");
+    c_nEqNull->SaveAs("c_nEqNull.png");
+    c_nEqNull->SaveAs("c_nEqNull.pdf");
+    c_nEqNull->SaveAs("c_nEqNull.C");
+    delete c_nEqNull;
+
+    TCanvas *c_nEqTwo = new TCanvas("c_nEqTwo", "", 4000, 3000);
+    h_nEqTwo->Draw("colz");
+    c_nEqTwo->SaveAs("c_nEqTwo.png");
+    c_nEqTwo->SaveAs("c_nEqTwo.pdf");
+    c_nEqTwo->SaveAs("c_nEqTwo.C");
+    delete c_nEqTwo;
+
     return 0;
 
 }
